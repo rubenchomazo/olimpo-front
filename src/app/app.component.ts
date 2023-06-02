9aceed35dc3c902ebd9f7cb00740d292f5bef381
rubenchomazo <58478880+rubenchomazo@users.noreply.github.com>
2023-06-02 17:28:06 -0500

    [CREATE] Create tailwind implementation for the styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'olimpus-front';
+  backgroundPage: any;
+  ngOnInit() {
+    // Get the background page element
+    this.backgroundPage = document.querySelector('.background-page');
+    // Add a scroll event listener to the background page
+    this.backgroundPage.addEventListener('scroll', this.onScroll.bind(this));
+  }
+
+  onScroll() {
+    this.backgroundPage.addEventListener('scroll', this.onScroll.bind(this));
+  }
 }
